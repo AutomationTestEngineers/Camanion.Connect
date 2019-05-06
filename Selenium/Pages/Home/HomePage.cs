2940f873170a2b4cb700a50f4f1374afd71ac392
Automation <User@DESKTOP-UNECDJV>
2019-05-06 21:27:14 +0530

    committing Changes

--- Selenium/Pages/Home/HomePage.cs
+++ Selenium/Pages/Home/HomePage.cs
@@ -3,6 +3,7 @@ using OpenQA.Selenium;
 using Selenium.Pages;
 using Selenium.Pages.Intake;
 using SeleniumExtras.PageObjects;
+using SeleniumExtras.WaitHelpers;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -17,7 +18,10 @@ namespace Selenium
         public HomePage(IWebDriver driver) : base(driver) { }
 
         [FindsBy]
-        private IWebElement newIntake = null, editAnimal=null;
+        private IWebElement newIntake = null, editAnimal=null, searchValue=null;
+
+        [FindsBy(How = How.XPath, Using = "//a[@class='QuickAdd-button']")]
+        private IWebElement addBtn = null;
 
         [FindsBy(How = How.XPath, Using = "//header[@id='shellHeader']/nav/div/div[2]/div/ul/li/a")]
         private IWebElement email = null;
@@ -25,16 +29,28 @@ namespace Selenium
         [FindsBy(How = How.XPath, Using = "//div[@class='input-group inline-input']/input")]
         private IWebElement searchInput = null;
 
-        [FindsBy(How = How.XPath, Using = "//div[@class='input-group inline-input']/span/button")]
-        private IWebElement searchButton = null;        
+        //[FindsBy(How = How.XPath, Using = "//div[@class='input-group inline-input']/span/button")]
+        //private IWebElement searchButton = null;
 
-        [FindsBy(How = How.XPath, Using = "//div[@id='mainmenu']//button/i[@class='icon icon-setting']")]
+        [FindsBy(How = How.XPath, Using = "//button[@class='SearchBar-button']")]
+        private IWebElement searchButton = null;
+
+        //[FindsBy(How = How.XPath, Using = "//div[@id='mainmenu']//button/i[@class='icon icon-setting']")]
+        //private IWebElement gearIcon = null;  
+
+        [FindsBy(How = How.XPath, Using = "(//*[@role='presentation'])[last()]")]
         private IWebElement gearIcon = null;
+
+        [FindsBy(How = How.XPath, Using = "//div[@class='Account-container']/div")]
+        private IList<IWebElement> selection = null;
 
         [FindsBy(How = How.XPath, Using = "//li[2]//a[contains(text(),'Change Shelter Selection')]")]
         private IWebElement changeShelter = null;
 
-        [FindsBy(How = How.XPath, Using = "//li[2]//a[contains(text(),'Admin')]")]
+        //[FindsBy(How = How.XPath, Using = "//li[2]//a[contains(text(),'Admin')]")]
+        //private IWebElement admin = null;
+
+        [FindsBy(How = How.LinkText, Using = "/admin")]
         private IWebElement admin = null;
 
         [FindsBy(How = How.XPath, Using = "//button[contains(text(),'OK')]")]
@@ -43,20 +59,27 @@ namespace Selenium
         [FindsBy(How = How.XPath, Using = "//tbody/tr/td[3]")]
         private IList<IWebElement> searchList = null;
 
-        [FindsBy(How = How.XPath, Using = "(//div[@id='mainmenu']//span[@class='input-group-btn']//i)[2]")]
-        private IWebElement activeOnly = null;
+        //[FindsBy(How = How.XPath, Using = "(//div[@id='mainmenu']//span[@class='input-group-btn']//i)[2]")]
+        //private IWebElement activeOnly = null;
 
-        [FindsBy(How = How.CssSelector, Using = "#mainmenu > form > div > div > div > div > select > option:nth-child(1)")]
-        private IWebElement searchDropDown = null;
+        [FindsBy(How = How.XPath, Using = "(//div[@class='SearchBar-checkbox'])[1]//span[1]")]
+        private IWebElement activeOnly = null; 
+
+        //[FindsBy(How = How.CssSelector, Using = "#mainmenu > form > div > div > div > div > select > option:nth-child(1)")]
+        //private IWebElement searchDropDown = null;
 
 
 
         public void ChangeShelter(string shelterName)
         {
             gearIcon.ClickCustom(driver);
-            changeShelter.ClickCustom(driver);
-            FindBy(By.XPath($"//strong[contains(text(),'{shelterName}')]"),10).ClickCustom(driver);
-            ok.ClickCustom(driver);
+            Sleep(500);
+            selection.Where(t => t.Text.Contains("Change Shelter Location")).FirstOrDefault().ClickCustom(driver);
+
+            //gearIcon.ClickCustom(driver);
+            //changeShelter.ClickCustom(driver);
+            //FindBy(By.XPath($"//strong[contains(text(),'{shelterName}')]"),10).ClickCustom(driver);
+            //ok.ClickCustom(driver);
         }        
 
         public string GetUserName()
@@ -86,10 +109,20 @@ namespace Selenium
             Sleep(1000);
             if (searchName == null)
                 searchName = Parameter.Get<string>("AnimalName");
-            activeOnly.ClickCustom(driver);
-            searchDropDown.ClickCustom(driver);
-            searchInput.SendKeysWrapper(searchName, driver);
+
+            ClickActiveOnly();
+            FindBy(By.XPath("//div[@id='searchType']/div")).ClickCustom(driver);
+            FindBy(By.XPath("(//div[text()='Animal Name'])[last()]")).ClickCustom(driver);
+
+            //searchDropDown.ClickCustom(driver);
+            searchValue.SendKeysWrapper(searchName, driver);
             searchButton.ClickCustom(driver);
+        }
+
+        public void ClickActiveOnly()
+        {
+            searchValue.ClickCustom(driver);
+            activeOnly.ClickCustom(driver);
         }
 
         public string GetAnimalName()
@@ -101,7 +134,7 @@ namespace Selenium
         public void ClickPencilIcon()
         {
             FindBy(By.XPath("(//span[@class='glyphicon glyphicon-pencil'])[1]")).ClickCustom(driver);
-            Sleep(2000);
+            Sleep(3000);
         }
         public ProfilePage EditAnimal()
         {
@@ -109,10 +142,17 @@ namespace Selenium
             return new ProfilePage(driver);
         }
         
-        public void DeleteRecentOutCome()
+        public AdministrationPage SelectAdmin()
         {
             gearIcon.ClickCustom(driver);
             admin.ClickCustom(driver);
+            return new AdministrationPage(driver);
+        }
+
+        public NewIntakePage ClickAdd()
+        {
+            addBtn.ClickCustom(driver);
+            return new NewIntakePage(driver);
         }
     }
 }
