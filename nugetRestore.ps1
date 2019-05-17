4205a24b46b80040baaccf09846e45668af36a8f
AutomationTestEngineers <49543318+AutomationTestEngineers@users.noreply.github.com>
2019-05-17 18:41:26 +0530

    Create nugetRestore.ps1
    To Restore the nuget gallery

---
+++ nugetRestore.ps1
@@ -0,0 +1,2 @@
+$args = @("restore", "C:\Users\pradeep.chinthala\Downloads\Campanion.Connect-Dev\Campanion.Connect\Companion.Connect.Automation.sln")
+& "C:\Users\pradeep.chinthala\Downloads\nuget.exe" $args
